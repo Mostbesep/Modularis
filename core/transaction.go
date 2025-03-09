d4bc6402aafeda7312da08fa74ae5850cf4df1b2
Mostbesep <mostafa.sep57@gmail.com>
2025-03-09 12:52:11 +0330

    transaction verify

--- core/transaction.go
+++ core/transaction.go
@@ -1,6 +1,14 @@
 package core
 
-import "github.com/Mostbesep/Modularis/crypto"
+import (
+	"errors"
+	"github.com/Mostbesep/Modularis/crypto"
+)
+
+var (
+	TransactionNotSignedErr = errors.New("transaction not signed")
+	InvalidSignatureErr     = errors.New("transaction signature or data is invalid")
+)
 
 type Transaction struct {
 	Data      []byte
@@ -16,4 +24,15 @@ func (tx *Transaction) Sign(privateKey crypto.PrivateKey) error {
 	tx.publicKey = privateKey.PublicKey()
 	tx.signature = sig
 	return nil
+}
+
+func (tx *Transaction) Verify() (bool, error) {
+	if tx.signature == nil {
+		return false, TransactionNotSignedErr
+	}
+	result := tx.signature.Verify(tx.publicKey, tx.Data)
+	if !result {
+		return false, InvalidSignatureErr
+	}
+	return result, nil
 }
--- core/transaction_test.go
+++ core/transaction_test.go
@@ -14,5 +14,37 @@ func TestTransaction_Sign(t *testing.T) {
 	prvKey := crypto.GeneratePrivateKey()
 	err := tx.Sign(prvKey)
 	assert.NoError(t, err)
-	assert.True(t, tx.signature.Verify(tx.publicKey, data))
+	assert.NotNil(t, tx.signature)
+	assert.NotNil(t, tx.publicKey)
+}
+
+func TestTransaction_Verify(t *testing.T) {
+	data := []byte("foo")
+	tx := &Transaction{
+		Data: data,
+	}
+	verify, err := tx.Verify()
+	assert.ErrorIs(t, err, TransactionNotSignedErr)
+	assert.False(t, verify)
+
+	prvKey := crypto.GeneratePrivateKey()
+	err = tx.Sign(prvKey)
+	assert.NoError(t, err)
+	verify, err = tx.Verify()
+	assert.NoError(t, err)
+	assert.True(t, verify)
+
+	otherPrvKey := crypto.GeneratePrivateKey()
+	tx.publicKey = otherPrvKey.PublicKey()
+	verify, err = tx.Verify()
+	assert.ErrorIs(t, err, InvalidSignatureErr)
+	assert.False(t, verify)
+
+	tx.Data = []byte("foo")
+	tx.Sign(prvKey)
+	tx.Data = []byte("bar")
+	verify, err = tx.Verify()
+	assert.ErrorIs(t, err, InvalidSignatureErr)
+	assert.False(t, verify)
+
 }
